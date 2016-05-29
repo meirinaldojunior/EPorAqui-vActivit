1cc423823458f3211553101ea08f750958e17521
Meirinaldo Junior <meirinaldo.junior0@gmail.com>
2016-05-29 14:42:51 -0300

    Junção da tela cadastro com a de login, implementação dos webViews do HTML para página de login e cadastro!
    (Ao invés de terem duas views, existe apenas uma view com tabs para login e cadastro!).

--- app/src/main/java/com/meirinaldojunior/eporaqui_vactivit/LoginTurista.java
+++ app/src/main/java/com/meirinaldojunior/eporaqui_vactivit/LoginTurista.java
@@ -1,15 +1,15 @@
 package com.meirinaldojunior.eporaqui_vactivit;
 
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-
-import com.meirinaldojunior.eporaqui_vactivit.cadastros.Cad_guia;
-import com.meirinaldojunior.eporaqui_vactivit.cadastros.Cad_turista;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class LoginTurista extends AppCompatActivity {
 
@@ -20,11 +20,36 @@ public class LoginTurista extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setAppCacheMaxSize(5 * 1024 * 1024); // 5MB
+        webView.getSettings().setAppCachePath(getApplicationContext().getCacheDir().getAbsolutePath());
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/loginCadastroTURISTA/index.html");
+
+
+        if (!isNetworkAvailable()) { // loading offline
+            Toast.makeText(this, "Você não está sem conexão, não conseguirá utilizar esta funcionalidade.", Toast.LENGTH_LONG);
+
+        }
+
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
+        });
+
     }
 
-
-    public void chamarCadastroTur(View view){
-        startActivity(new Intent(this, Cad_turista.class));
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
 }
