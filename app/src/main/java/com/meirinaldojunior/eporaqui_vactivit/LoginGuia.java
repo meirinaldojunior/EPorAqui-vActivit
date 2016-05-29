f2d8d3aa83a23400aef53f51bb0798b3b1078e0d
Meirinaldo Junior <meirinaldo.junior0@gmail.com>
2016-05-29 02:01:50 -0300

    Implementação telas e chamada para tela cadastro: *Login -Guia e turista;

--- app/src/main/java/com/meirinaldojunior/eporaqui_vactivit/LoginGuia.java
+++ app/src/main/java/com/meirinaldojunior/eporaqui_vactivit/LoginGuia.java
@@ -1,11 +1,14 @@
 package com.meirinaldojunior.eporaqui_vactivit;
 
+import com.meirinaldojunior.eporaqui_vactivit.cadastros.*;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class LoginGuia extends AppCompatActivity {
 
@@ -16,14 +19,10 @@ public class LoginGuia extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    }
+
+    public void chamarCadastro(View view){
+        startActivity(new Intent(this, Cad_guia.class));
     }
 
 }
