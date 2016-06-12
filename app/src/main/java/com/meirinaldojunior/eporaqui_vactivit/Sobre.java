ba0de087ad4967229ddbe9336d7dd9b07834bcea
Meirinaldo Junior <meirinaldo.junior0@gmail.com>
2016-06-11 23:29:05 -0300

    Build versão 0.0.2 (Alpha)

--- app/src/main/java/com/meirinaldojunior/eporaqui_vactivit/Sobre.java
+++ app/src/main/java/com/meirinaldojunior/eporaqui_vactivit/Sobre.java
@@ -5,6 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class Sobre extends AppCompatActivity {
@@ -15,8 +17,24 @@ public class Sobre extends AppCompatActivity {
         setContentView(R.layout.activity_sobre);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
     }
 
 }
